e208e92c78ebc2d5dbd5fdde7387ab54795425d6
H.zwaan <hugo.zwaan.zwaan@hva.nl>
2020-10-07 12:48:17 +0200

    Meer dynamische naamgeving aan wav files

--- src/code/stt.py
+++ src/code/stt.py
@@ -1,14 +1,21 @@
 import speech_recognition as sr
 import sounddevice as sd
 import soundfile as sf
-from datetime import datetime
+import pathlib
+import glob
+import os
+from datetime import datetime, timedelta
 
 
 class SpeechToText:
-    def __init__(self, path):
-        self.path = path
+    # def __init__(self):
+    def find_last_modified_recording(self):
+        return max(glob.iglob(R"./src/assets/data/recordings/*.wav"), key=os.path.getmtime)
 
-    def classify_wav(self, path):
+    def classify_wav(self, path=None):
+        if path is None:
+            path = self.find_last_modified_recording() 
+
         r = sr.Recognizer()
         try:
             with sr.WavFile(path) as source:
@@ -17,7 +24,6 @@ class SpeechToText:
             raise e
 
         try:
-            global txt
             txt = r.recognize_google(audio, None, "nl_NL")
             print(f"You said: {txt}")
             return txt
@@ -25,53 +31,48 @@ class SpeechToText:
             print("Google Speech Recognition could not understand audio")
             return None
         except sr.RequestError as e:
-            print("Could not request results from Google Speech Recognition service; {0}".format(e))
+            print(
+                "Could not request results from Google Speech Recognition service; {0}".format(
+                    e
+                )
+            )
             return None
 
-    def record_wav(self):
+    # fs = 44100 - 48000
+    # seconds = 10
+    def record_wav(
+        self,
+        fs=44100,
+        second=10,
+        filename=f'recording_{str(datetime.now().strftime("%d-%m-%Y_%H-%M-%S"))}.wav',
+    ):
         # get input and output devices
         devices = sd.query_devices()
         input_d, output_d = sd.default.device
-        input_device = devices[input_d]['name']
-        output_device = devices[output_d]['name']
-
-        print(f'devices: {devices}')
-        print(f'input: {input_device}, output: {output_device}')
+        print(f"devices: {devices}")
 
         # give user the possibility to change settings
         while True:
-            corr = input('Setting correct? (y/n)')
-            if corr == 'y':
+            print(f"input: {devices[input_d]['name']}")
+            corr = input("input correct? (y/n)")
+            if corr == "y":
                 break
             else:
-                put = input('input or output? (i/o)')
-                num = input('correct number?')
-                if put == 'i':
-                    input_d = int(num)
-                elif put == 'o':
-                    output_d = int(num)
-                else:
-                    print('incorrect try again')
+                input_d = int(input("correct number?"))
 
         # set new output and input devices
-        print(output_d, input_d, type(output_d), type(input_d))
-        sd.default.device = input_d, output_d
+        sd.default.device = input_d
 
         # recording
-        fs = 44100
-        second = 10
         record_voice = sd.rec(int(second * fs), samplerate=fs, channels=2)
         sd.wait()
-        sf.write(self.path, record_voice, fs)
+        sf.write(f"./src/assets/data/recordings/{filename}", record_voice, fs)
 
     def record_and_classify(self):
         self.record_wav()
-        self.classify_wav(self.path)
+        self.classify_wav()
 
 
-#filename = f'recording_{str(datetime.now().strftime("%d-%m-%Y_%H-%M-%S"))}.wav'
-filename = 'recording_06-10-2020_19-38-33.wav'
-filelocation = f'../assets/data/recordings/{filename}'
-
-stt = SpeechToText(filelocation)
-stt.classify_wav(filelocation)
+stt = SpeechToText()
+stt.classify_wav()
+# stt.classify_wav()
--- src/code/utils.py
+++ src/code/utils.py
@@ -2,22 +2,22 @@ import pathlib
 from pydub import AudioSegment
 from pathlib import Path
 
+
 def get_current_dir():
     return Path.cwd()
 
-class Utils:
 
+class Utils:
     @staticmethod
     def mp3_to_wav():
         for path in pathlib.Path("../assets/data/sounds_mp3/sentences").iterdir():
             sound = AudioSegment.from_mp3(path)
-            filename = str(path).split('\\')[-1].split('.')[0]
+            filename = str(path).split("\\")[-1].split(".")[0]
             dest = f"../assets/data/sounds_wav/sentences/{filename}.wav"
             sound.export(dest, format="wav")
 
         for path in pathlib.Path("../assets/data/sounds_mp3/words").iterdir():
             sound = AudioSegment.from_mp3(path)
-            filename = str(path).split('\\')[-1].split('.')[0]
+            filename = str(path).split("\\")[-1].split(".")[0]
             dest = f"../assets/data/sounds_wav/words/{filename}.wav"
             sound.export(dest, format="wav")
-
