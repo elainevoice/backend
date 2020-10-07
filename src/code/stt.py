e78c03860db1c1a679510c967680768a6d23959c
lodu <ludo@ludobak.nl>
2020-10-07 13:28:03 +0200

    optimized Hugo's code and verison pinned packages

--- src/code/stt.py
+++ src/code/stt.py
@@ -9,69 +9,61 @@ from datetime import datetime, timedelta
 
 class SpeechToText:
     # def __init__(self):
-    def find_last_modified_recording(self):
+    def _find_last_modified_recording(self):
         return max(
             glob.iglob(R"./src/assets/data/recordings/*.wav"), key=os.path.getmtime
         )
 
-    def classify_wav(self, path=None):
-        if path is None:
-            path = self.find_last_modified_recording()
+    def _select_input_device(self):
+        devices = sd.query_devices()
+        input_d, _ = sd.default.device
 
-        r = sr.Recognizer()
+        print(f"devices: {devices}")
+        corr = "n"
+        while corr == "n":
+            input_d = int(input("Input correct input: "))
+            print(f"input: {devices[input_d]['name']}")
+            corr = input("input correct? (y/n) ")
+        sd.default.device = input_d
+
+    def classify_wav(self, path=None):
+        path = self._find_last_modified_recording() if path is None else path
+
+        recognizer = sr.Recognizer()
         try:
             with sr.WavFile(path) as source:
-                audio = r.record(source)
+                audio = recognizer.record(source)
         except Exception as e:
             raise e
 
         try:
-            txt = r.recognize_google(audio, None, "nl_NL")
+            txt = recognizer.recognize_google(audio, None, "nl_NL")
             print(f"You said: {txt}")
             return txt
         except sr.UnknownValueError:
-            print("Google Speech Recognition could not understand audio")
-            return None
+            raise ValueError("Google Speech Recognition could not understand audio")
         except sr.RequestError as e:
-            print(
-                "Could not request results from Google Speech Recognition service; {0}".format(
-                    e
-                )
+            raise Exception(
+                f"Could not request results from Google Speech Recognition service: {e}"
             )
-            return None
 
-    # fs = 44100 - 48000
-    # seconds = 10
-    def record_wav(
+    def record_and_save_wav(
         self,
         fs=44100,
         second=10,
         filename=f'recording_{str(datetime.now().strftime("%d-%m-%Y_%H-%M-%S"))}.wav',
     ):
-        # get input and output devices
-        devices = sd.query_devices()
-        input_d, output_d = sd.default.device
-        print(f"devices: {devices}")
 
-        # give user the possibility to change settings
-        while True:
-            print(f"input: {devices[input_d]['name']}")
-            corr = input("input correct? (y/n)")
-            if corr == "y":
-                break
-            else:
-                input_d = int(input("correct number?"))
+        self._select_input_device()
 
-        # set new output and input devices
-        sd.default.device = input_d
-
-        # recording
+        save_path = f"./src/assets/data/recordings/{filename}"
         record_voice = sd.rec(int(second * fs), samplerate=fs, channels=2)
         sd.wait()
-        sf.write(f"./src/assets/data/recordings/{filename}", record_voice, fs)
+        sf.write(save_path, record_voice, fs)
+        print(f"Saved at: {save_path}")
 
     def record_and_classify(self):
-        self.record_wav()
+        self.record_and_save_wav()
         self.classify_wav()
 
 
