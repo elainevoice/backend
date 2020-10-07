0dfd4ad62da9d831cea4abbbded83493dab447da
lodu <ludo@ludobak.nl>
2020-10-07 13:58:10 +0200

    black

--- src/code/tts.py
+++ src/code/tts.py
@@ -4,4 +4,4 @@ from config import lang
 text = str(input("What do you want to tts? "))
 
 tts = gTTS(text=text, lang=lang)
-tts.save(f'tts_{str(datetime.now().strftime("%d-%m-%Y_%H-%M-%S"))}.wav')
+tts.save(f'tts_{str(datetime.now().strftimble("%d-%m-%Y_%H-%M-%S"))}.wav')
