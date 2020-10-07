1b549cbe3650d8ab396413265e0a368bbb7c8b32
Quirijn Langedijk <github@quirijnlangedijk.nl>
2020-10-07 13:28:59 +0200

    betere path

--- src/code/utils.py
+++ src/code/utils.py
@@ -7,12 +7,10 @@ class Utils:
     def mp3_to_wav():
         for path in pathlib.Path("../assets/data/sounds_mp3/sentences").iterdir():
             sound = AudioSegment.from_mp3(path)
-            filename = str(path).split("\\")[-1].split(".")[0]
-            dest = f"../assets/data/sounds_wav/sentences/{filename}.wav"
+            dest = f"../assets/data/sounds_wav/sentences/{path.stem}.wav"
             sound.export(dest, format="wav")
 
         for path in pathlib.Path("../assets/data/sounds_mp3/words").iterdir():
             sound = AudioSegment.from_mp3(path)
-            filename = str(path).split("\\")[-1].split(".")[0]
-            dest = f"../assets/data/sounds_wav/words/{filename}.wav"
+            dest = f"../assets/data/sounds_wav/words/{path.stem}.wav"
             sound.export(dest, format="wav")
