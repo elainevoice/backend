7a5ec559a5e226cb3b450a1822919d0641014156
Iodu <48859312+Iodu@users.noreply.github.com>
2020-10-07 14:06:20 +0200

    Stt (#1)
    * Added google stt
    
    * Added mp3s and wavs
    
    * Update README.md
    
    * input from mick to wav
    
    * Code leesbaar gemaakt
    
    * added get_curr_dir func to utils
    
    * revert last commit, waar de fuck is git revert in github interface
    
    * Meer dynamische naamgeving aan wav files
    
    * formatting/black .
    
    * optimized Hugo's code and verison pinned packages
    
    * betere path
    
    * added black code formatter
    
    * changed readme.md
    
    * changed readme.md
    
    * md \n's zijn kut
    
    * fuck you readme.md
    
    * sorted import
    
    * added config file and added lang in it
    
    Co-authored-by: Quirijn Langedijk <github@quirijnlangedijk.nl>
    Co-authored-by: H.zwaan <hugo.zwaan.zwaan@hva.nl>

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# scripts
+# Scripts
+## This repo contains several scripts used for Elaine Voice
+
+### Install
+`git clone <REPO>`
+`cd <FOLDER>`
+
+`python -m venv venv`
+then activate venv
+
+then install req's
+`python -m pip install -r requirements.txt`
+
+### Formatting
+`black .` for all files in and under dir
+
+
+`black <FILE>` for specific file
+
---
+++ src/code/utils.py
@@ -0,0 +1,17 @@
+import pathlib
+
+from pydub import AudioSegment
+
+
+class Utils:
+    @staticmethod
+    def mp3_to_wav():
+        for path in pathlib.Path("../assets/data/sounds_mp3/sentences").iterdir():
+            sound = AudioSegment.from_mp3(path)
+            dest = f"../assets/data/sounds_wav/sentences/{path.stem}.wav"
+            sound.export(dest, format="wav")
+
+        for path in pathlib.Path("../assets/data/sounds_mp3/words").iterdir():
+            sound = AudioSegment.from_mp3(path)
+            dest = f"../assets/data/sounds_wav/words/{path.stem}.wav"
+            sound.export(dest, format="wav")
