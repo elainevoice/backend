c919e239cfd905e6cb81bf25fc10cb68f7140901
H.zwaan <hugo.zwaan.zwaan@hva.nl>
2020-10-08 15:18:40 +0200

    gui looks normal

--- src/code/config.py
+++ src/code/config.py
@@ -1,4 +1,8 @@
 lang = "nl"
+application_name = 'Elaine Voice'
 default_fs = 44100
 default_length_recording = 10
-default_window_size = "1024x600"
+default_window_size = "600x200"
+default_btn_width = 15
+default_btn_height = 2
+default_dropdown_width = 60
--- src/code/interface.py
+++ src/code/interface.py
@@ -1,20 +1,19 @@
 from tkinter import *
 from stt import SpeechToText
-from config import default_window_size
+from config import default_window_size, application_name, default_btn_width, default_btn_height, default_dropdown_width
 
 class Interface:
     gui = None
     def __init__(self, windowsize = default_window_size):
-        self.gui = Tk(className='Elaine Voice')
+        self.gui = Tk(className = application_name)
         self.gui.geometry(windowsize)
 
     def start(self):
         def confirm():
-            new_device = var.get().replace('{','').replace('}','')[0]
-            stt.set_input_device(int(new_device))
+            stt.set_input_device(int(var.get().replace('{','').replace('}','')[0]))
         
         def record():
-            stt.record_and_save_wav()
+            stt.record_and_classify()
         
         stt = SpeechToText()
         current_device, device_list = stt.select_input_device()
@@ -23,14 +22,15 @@ class Interface:
         var = StringVar(self.gui)
         var.set(current_device)
         w = OptionMenu(self.gui, var, *device_list)
+        w.config(width=default_dropdown_width)
         w.pack()
 
         # create confirm dropdown
-        confrm = Button(self.gui, text="Input Device", command=confirm)
+        confrm = Button(self.gui, text="Input Device", command=confirm, width=default_btn_width, height=default_btn_height)
         confrm.pack()  
 
         # create recording button
-        rec = Button(self.gui, text="record", command=record)
+        rec = Button(self.gui, text="record", command=record, width=default_btn_width, height=default_btn_height)
         rec.pack()
 
         mainloop() 
