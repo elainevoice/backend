9588b1f159150002a0a8ee462864f32cf2a4c40f
H.zwaan <hugo.zwaan.zwaan@hva.nl>
2020-10-09 09:53:30 +0200

    WIP 2

--- src/code/audio_player.py
+++ src/code/audio_player.py
@@ -7,11 +7,14 @@ import soundfile as sf
 class AudioPlayer:
     def _find_last_modified_result(self):
         return max(
-            glob.iglob(R"./src/assets/data/recordings/*.wav"), key=os.path.getmtime # R"./src/assets/data/results/*.wav" R"./src/assets/data/recordings/*.wav"
+            glob.iglob(R"./src/assets/data/results/*.wav"), key=os.path.getmtime
         )
     
     def play_wav(self, wav = None):
-        wav = self._find_last_modified_result() if wav is None else wav
-        data, fs = sf.read(wav, dtype='float32')  
-        sd.play(data, fs)
-        status = sd.wait()
+        try:
+            wav = self._find_last_modified_result() if wav is None else wav
+            data, fs = sf.read(wav, dtype='float32')
+            sd.play(data, fs)
+            status = sd.wait()
+        except Exception as e:
+            raise e
--- src/code/interface.py
+++ src/code/interface.py
@@ -1,10 +1,11 @@
 import sounddevice as sd
+import config 
 
 from tkinter import *
 from stt import SpeechToText
 from tts import textToSpeech
 from audio_player import AudioPlayer
-from config import default_window_size, application_name, default_btn_width, default_btn_height, default_dropdown_width
+
 
 class Interface:
     gui = None
@@ -14,8 +15,8 @@ class Interface:
     tts = None
     ap = None
 
-    def __init__(self, windowsize = default_window_size):
-        self.gui = Tk(className = application_name)
+    def __init__(self, windowsize = config.default_window_size):
+        self.gui = Tk(className = config.application_name)
         self.gui.geometry(windowsize)
         self.stt = SpeechToText()
         self.tts = textToSpeech()
@@ -29,7 +30,7 @@ class Interface:
             self._set_output_device(int(outp.get().replace('{','').replace('}','')[0]))
         
         def record():
-            self.stt.record_and_classify()
+            self.stt.record_and_save_wav()
             self.tts.create_wav()
             self.ap.play_wav()
         
@@ -37,29 +38,23 @@ class Interface:
         current_input_device, current_output_device, input_device, output_device = self._possible_device()
 
         # create input dropdown
-        inp = StringVar(self.gui)
-        inp.set(current_input_device)
-        inp_drop = OptionMenu(self.gui, inp, *input_device)
-        inp_drop.config(width=default_dropdown_width)
+        inp, inp_drop = self._create_dropdown(current_input_device, input_device)
         inp_drop.pack()
 
         # create input confirm
-        confrm = Button(self.gui, text="Input Device", command=confirm_input, width=default_btn_width, height=default_btn_height)
+        confrm = self._create_button("Input Device",confirm_input)
         confrm.pack()
 
         # create output dropdown
-        outp = StringVar(self.gui)
-        outp.set(current_output_device)
-        outp_drop = OptionMenu(self.gui, outp, *output_device)
-        outp_drop.config(width=default_dropdown_width)
+        outp, outp_drop = self._create_dropdown(current_output_device, output_device)
         outp_drop.pack()
 
         # create output confirm
-        confrm = Button(self.gui, text="Output Device", command=confirm_output, width=default_btn_width, height=default_btn_height)
+        confrm =  self._create_button("Output Device",confirm_output)
         confrm.pack()    
 
         # create recording button
-        rec = Button(self.gui, text="Record", command=record, width=default_btn_width, height=default_btn_height)
+        rec = self._create_button("Record",record)
         rec.pack()
 
         mainloop() 
@@ -83,6 +78,16 @@ class Interface:
         self.selected_output_device = o_d
         print(f'new input: {self.selected_input_device}, new output: {o_d}')
         sd.default.device = self.selected_input_device, o_d
+
+    def _create_dropdown(self, current, options):
+        var = StringVar(self.gui)
+        var.set(current)
+        drop = OptionMenu(self.gui, var, *options)
+        drop.config(width=config.default_dropdown_width)
+        return var, drop
+
+    def _create_button(self, text, command):
+        return Button(self.gui, text=text, command=command, width=config.default_btn_width, height=config.default_btn_height)
 
 i = Interface()
 i.start()
