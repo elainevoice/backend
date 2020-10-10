5cb32c68a7524d692c751dd2d6ca611a95dc03bd
lodu <ludo@ludobak.nl>
2020-10-10 15:16:38 +0200

    removed Docker + more mvc

--- src/app/models/audio_player.py
+++ src/app/models/audio_player.py
@@ -1,17 +1,18 @@
 import glob
 import os
+
 import pyglet
 import sounddevice as sd
 import soundfile as sf
+from helpers import find_last_modified_result
+
 
 class AudioPlayer:
     count = 0
-    def _find_last_modified_result(self):
-        return max(glob.iglob(R"./src/assets/data/results/*.wav"), key=os.path.getmtime)
-    
+   
     def play_wav(self, wav = None):
         try:
-            wav = self._find_last_modified_result() if wav is None else wav
+            wav = find_last_modified_result() if wav is None else wav
             data, fs = sf.read(wav, dtype='float32')
             sd.play(data, fs)
             status = sd.wait()
