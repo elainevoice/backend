7eb5a085273f04800fd5ea9cc5867881e41ea06f
lodu <ludo@ludobak.nl>
2020-10-10 17:50:49 +0200

    fixed bug where /docs would crash

--- src/app/routes.py
+++ src/app/routes.py
@@ -15,15 +15,15 @@ def home():
 
 
 @router.post('/recognize_audio_disk')
-def post_recording_disk(file: UploadFile = File(...), response_class=JSONResponse):
+def post_recording_disk(file: UploadFile = File(...)):
     try:
         spooled_temp_file = file.file
         path_name, text = controller.stt_recognize_binary_audio_on_disk(
             spooled_temp_file)
-        return {
+        return JSONResponse({
             "path": path_name,
             "text": text,
-        }
+        })
     # To do  test what exceptions to actually catch
     except Exception as e:
         print(e)
@@ -32,19 +32,19 @@ def post_recording_disk(file: UploadFile = File(...), response_class=JSONRespons
 
 # Niet helemaal in memory want als UploadFile te groot is wordt het op schijf opgeslagen, zie https://fastapi.tiangolo.com/tutorial/request-files/
 @router.post('/recognize_audio_memory')
-def post_recording_memory(file: UploadFile = File(...), response_class=PlainTextResponse):
+def post_recording_memory(file: UploadFile = File(...)):
     try:
         spooled_temp_file = file.file
         text = controller.stt_recognize_binary_audio_in_memory(
             spooled_temp_file)
-        return text
+        return PlainTextResponse(text)
     # To do  test what exceptions to actually catch
     except Exception as e:
         raise HTTPException(status_code=418, detail=str(e))
 
 
 @router.get('/create_audio')
-def create_audio_from_text(text: str, response_class=FileResponse):
+def create_audio_from_text(text: str):
     try:
         audio_path = controller.tts_create_audio_from_text(text)
 
