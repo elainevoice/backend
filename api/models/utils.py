6e000f08e24a2399eb0c08858ac2ee2381ea9070
Quirijn Langedijk <github@quirijnlangedijk.nl>
2020-10-19 11:05:45 +0200

    Nederlandse shit (#8)

--- api/models/utils.py
+++ api/models/utils.py
@@ -1,6 +1,7 @@
 import pathlib
-
+import os
 from pydub import AudioSegment
+import pandas as pd
 
 
 class Utils:
@@ -15,3 +16,21 @@ class Utils:
             sound = AudioSegment.from_mp3(path)
             dest = f"../assets/data/sounds_wav/words/{path.stem}.wav"
             sound.export(dest, format="wav")
+
+    @staticmethod
+    def rename_wav():
+        words = pd.read_csv('../assets/data/words_dict_nl.csv')
+        sentences = pd.read_csv('../assets/data/sentences_dict_nl.csv')
+
+        directory = "../assets/data/sounds_wav/sentences"
+        for filename in os.listdir(directory):
+            turks = filename[:-4].lower()
+            match = sentences.loc[sentences['turks'] == f"sounds/sentences/{turks}.mp3"]
+            nederlands = match['nederlands'].item().replace(' ', '_')
+            old_file = os.path.join(directory, filename)
+            new_filename = old_file.replace(turks, nederlands)
+            try:
+                os.rename(old_file, new_filename)
+            except:
+                pass
+
