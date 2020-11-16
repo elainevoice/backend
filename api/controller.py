fe4a100ca55a7a28f6324bef317677ed1a7fce51
Quirijn Langedijk <github@quirijnlangedijk.nl>
2020-11-16 13:26:57 +0100

    Het werkt niet

--- api/controller.py
+++ api/controller.py
@@ -3,15 +3,15 @@ from api.models.taco_adapter import TacoTronAdapter
 from api.models.tts import ttsAdapter
 
 
-def stt_recognize_binary_audio_on_disk(spooled_temp_file):
+async def stt_recognize_binary_audio_on_disk(spooled_temp_file):
     stt_adapter = sttAdapter()
-    path_name, text = stt_adapter.recognize_audio_disk(spooled_temp_file)
+    path_name, text = await stt_adapter.recognize_audio_disk(spooled_temp_file)
     return path_name, text
 
 
-def stt_recognize_binary_audio_in_memory(spooled_temp_file):
+async def stt_recognize_binary_audio_in_memory(spooled_temp_file):
     stt_adapter = sttAdapter()
-    return stt_adapter.recognize_audio_memory(spooled_temp_file)
+    return await stt_adapter.recognize_audio_memory(spooled_temp_file)
 
 
 def tts_create_audio_from_text(text):
@@ -23,4 +23,5 @@ def tts_create_audio_from_text(text):
 def text_to_tacotron_audio_file(text):
     tta = TacoTronAdapter()
     absolute_file_path = tta.generate_wav(text)
+    print(absolute_file_path)
     return absolute_file_path
--- api/routes.py
+++ api/routes.py
@@ -1,7 +1,11 @@
+import tempfile
+
 from fastapi import APIRouter, File, HTTPException, UploadFile
 from fastapi.responses import (FileResponse, HTMLResponse, JSONResponse,
                                PlainTextResponse)
+from pydantic.main import BaseModel
 from starlette.requests import Request
+import sys
 
 from api import controller
 from api.config import application_name
@@ -16,10 +20,10 @@ def home():
 
 
 @router.post('/recognize_audio_disk')
-def post_recording_disk(file: UploadFile = File(...)):
+async def post_recording_disk(file: UploadFile = File(...)):
     try:
         spooled_temp_file = file.file
-        path_name, text = controller.stt_recognize_binary_audio_on_disk(
+        path_name, text = await controller.stt_recognize_binary_audio_on_disk(
             spooled_temp_file)
         return JSONResponse({
             "path": path_name,
@@ -33,11 +37,10 @@ def post_recording_disk(file: UploadFile = File(...)):
 
 # Niet helemaal in memory want als UploadFile te groot is wordt het op schijf opgeslagen, zie https://fastapi.tiangolo.com/tutorial/request-files/
 @router.post('/recognize_audio_memory')
-def post_recording_memory(file: UploadFile = File(...)):
+async def post_recording_memory(file: UploadFile = File(...)):
     try:
         spooled_temp_file = file.file
-        text = controller.stt_recognize_binary_audio_in_memory(
-            spooled_temp_file)
+        text = await controller.stt_recognize_binary_audio_in_memory(spooled_temp_file)
         return PlainTextResponse(text)
     # To do  test what exceptions to actually catch
     except Exception as e:
@@ -58,7 +61,7 @@ def create_audio_from_text(text: str):
 def crack_create_audio_from_text(text: str, request: Request):
     try:
         audio_path = controller.tts_create_audio_from_text(text)
-        return str(request.url).split('/crack_create')[0] + "/crack_audio_oplossing?audio_name="+ audio_path
+        return str(request.url).split('/crack_create')[0] + "/crack_audio_oplossing?audio_name=" + audio_path
     # To do  test what exceptions to actually catch
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
@@ -75,4 +78,19 @@ def text_to_tacotron_audio_file(data: TextData):
         wav_audio_file_path = controller.text_to_tacotron_audio_file(data.text)
         return FileResponse(str(wav_audio_file_path))
     except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@router.post('/taco_audio')
+async def audio_to_tacotron_audio_file(file: UploadFile = File(...)):
+    try:
+        print(file.file)
+        spooled_temp_file = file.file
+        print(spooled_temp_file)
+        text = await controller.stt_recognize_binary_audio_in_memory(spooled_temp_file)
+        print('kkkkk')
+        wav_audio_file_path = controller.text_to_tacotron_audio_file(text)
+        return FileResponse(str(wav_audio_file_path))
+    except Exception as e:
+        print(e)
         raise HTTPException(status_code=500, detail=str(e))
