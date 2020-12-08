e0bfa5a3f89e181fb35bee4050094d44f03b43df
HugoZwaan <hugo.zwaan.zwaan@hva.nl>
2020-12-08 11:25:42 +0100

    api cleanup

--- api/routes.py
+++ api/routes.py
@@ -1,8 +1,6 @@
 from fastapi import APIRouter, File, HTTPException, UploadFile, Header
-from fastapi.responses import (FileResponse, HTMLResponse, JSONResponse,
-                               PlainTextResponse)
+from fastapi.responses import (FileResponse, HTMLResponse,)
 from pydantic.main import List
-from starlette.requests import Request
 
 from api import controller
 from api.config import application_name
@@ -20,67 +18,8 @@ def home():
     return f"<body><h1>API of {application_name}</h1></body>"
 
 
-@router.post("/recognize_audio_disk")
-async def post_recording_disk(file: UploadFile = File(...)):
-    try:
-        spooled_temp_file = file.file
-        path_name, text = await controller.stt_recognize_binary_audio_on_disk(
-            spooled_temp_file
-        )
-        return JSONResponse(
-            {
-                "path": path_name,
-                "text": text,
-            }
-        )
-    # To do  test what exceptions to actually catch
-    except Exception as e:
-        raise HTTPException(status_code=500, detail="Something went wrong")
-
-
-# Niet helemaal in memory want als UploadFile te groot is wordt het op schijf opgeslagen, zie https://fastapi.tiangolo.com/tutorial/request-files/
-@router.post("/recognize_audio_memory")
-async def post_recording_memory(file: UploadFile = File(...)):
-    try:
-        spooled_temp_file = file.file
-        text = await controller.stt_recognize_binary_audio_in_memory(spooled_temp_file)
-        return PlainTextResponse(text)
-    # To do  test what exceptions to actually catch
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-
-
-@router.post("/create_audio")
-def create_audio_from_text(text: str):
-    try:
-        audio_path = controller.tts_create_audio_from_text(text)
-        return FileResponse(audio_path)
-    # To do  test what exceptions to actually catch
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-
-
-@router.post("/crack_create_audio")
-def crack_create_audio_from_text(text: str, request: Request):
-    try:
-        audio_path = controller.tts_create_audio_from_text(text)
-        return (
-            str(request.url).split("/crack_create")[0]
-            + "/crack_audio_oplossing?audio_name="
-            + audio_path
-        )
-    # To do  test what exceptions to actually catch
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-
-
-@router.get("/crack_audio_oplossing")
-def crack_audio_oplossing(audio_name: str):
-    return FileResponse(audio_name)
-
-
 @router.post('/taco')
-def text_to_tacotron_audio_file(data: TTS_model, model = Header(None)):
+def text_to_tacotron_audio_file(data: TTSModel, model= Header(None)):
     try:
         wav_audio_file_path = controller.text_to_tacotron_audio_file(data.text, model)
         return FileResponse(str(wav_audio_file_path))
@@ -89,7 +28,7 @@ def text_to_tacotron_audio_file(data: TTS_model, model = Header(None)):
 
 
 @router.post('/taco_audio')
-async def audio_to_tacotron_audio_file(file: UploadFile = File(...), model = Header(None)):
+async def audio_to_tacotron_audio_file(file: UploadFile = File(...), model= Header(None)):
 
     try:
         bytes = await file.read()
