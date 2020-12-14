77a3ff264045cf855d35e5c8687c965747725b64
Xyrai <Xyraidev@gmail.com>
2020-12-14 13:39:41 +0100

    Refactored cleaners.py

--- api/models/utils/text/cleaners.py
+++ api/models/utils/text/cleaners.py
@@ -1,7 +1,6 @@
 from phonemizer.phonemize import phonemize
 from api.models.utils import hparams as hp
 
-
 """ from https://github.com/keithito/tacotron """
 
 '''
@@ -20,68 +19,67 @@ import re
 from unidecode import unidecode
 from .numbers import normalize_numbers
 
-
 # Regular expression matching whitespace:
 _whitespace_re = re.compile(r'\s+')
 
 # List of (regular expression, replacement) pairs for abbreviations:
 _abbreviations = [(re.compile('\\b%s\\.' % x[0], re.IGNORECASE), x[1]) for x in [
-  ('mrs', 'misess'),
-  ('mr', 'mister'),
-  ('dr', 'doctor'),
-  ('st', 'saint'),
-  ('co', 'company'),
-  ('jr', 'junior'),
-  ('maj', 'major'),
-  ('gen', 'general'),
-  ('drs', 'doctors'),
-  ('rev', 'reverend'),
-  ('lt', 'lieutenant'),
-  ('hon', 'honorable'),
-  ('sgt', 'sergeant'),
-  ('capt', 'captain'),
-  ('esq', 'esquire'),
-  ('ltd', 'limited'),
-  ('col', 'colonel'),
-  ('ft', 'fort'),
+    ('mrs', 'misess'),
+    ('mr', 'mister'),
+    ('dr', 'doctor'),
+    ('st', 'saint'),
+    ('co', 'company'),
+    ('jr', 'junior'),
+    ('maj', 'major'),
+    ('gen', 'general'),
+    ('drs', 'doctors'),
+    ('rev', 'reverend'),
+    ('lt', 'lieutenant'),
+    ('hon', 'honorable'),
+    ('sgt', 'sergeant'),
+    ('capt', 'captain'),
+    ('esq', 'esquire'),
+    ('ltd', 'limited'),
+    ('col', 'colonel'),
+    ('ft', 'fort'),
 ]]
 
 
 def expand_abbreviations(text):
-  for regex, replacement in _abbreviations:
-    text = re.sub(regex, replacement, text)
-  return text
+    for regex, replacement in _abbreviations:
+        text = re.sub(regex, replacement, text)
+    return text
 
 
 def expand_numbers(text):
-  return normalize_numbers(text)
+    return normalize_numbers(text)
 
 
 def lowercase(text):
-  return text.lower()
+    return text.lower()
 
 
 def collapse_whitespace(text):
-  return re.sub(_whitespace_re, ' ', text)
+    return re.sub(_whitespace_re, ' ', text)
 
 
 def convert_to_ascii(text):
-  return unidecode(text)
+    return unidecode(text)
 
 
 def basic_cleaners(text):
-  text = to_phonemes(text)
-  text = collapse_whitespace(text)
-  return text
+    text = to_phonemes(text)
+    text = collapse_whitespace(text)
+    return text
 
 
 def english_cleaners(text):
-  text = convert_to_ascii(text)
-  text = expand_numbers(text)
-  text = expand_abbreviations(text)
-  text = to_phonemes(text)
-  text = collapse_whitespace(text)
-  return text
+    text = convert_to_ascii(text)
+    text = expand_numbers(text)
+    text = expand_abbreviations(text)
+    text = to_phonemes(text)
+    text = collapse_whitespace(text)
+    return text
 
 
 def to_phonemes(text):
